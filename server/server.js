0c2fed8d7aa029e8bc3b01ee1b5a1dab7546527c
Chris Palka <cpalka87gmail.com>
2020-08-27 21:36:39 -0600

    Complete render of all modules

---
+++ server/server.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+
+const PORT = 3009;
+
+
+app.use((req, res, next) => {
+  console.log(req.url)
+  if (req.url === '/') {
+    res.redirect(301, req.url + '?productId=P001');
+  }
+  next();
+});
+
+app.use(express.static(path.join(__dirname, '../client')));
+
+app.listen(PORT, () => {
+  // eslint-disable-next-line no-console
+  console.log(`listening on port http://localhost:${PORT}`);
+});
